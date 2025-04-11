03f69441f73ad69bee66451eec355cca823e84fc
bosnar <dev.chadchai@gmail.comwd>
2025-04-11 08:30:50 +0700

    feat: create act and rft token

--- src/application/usecases/authentication.rs
+++ src/application/usecases/authentication.rs
@@ -1,8 +1,20 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
+use chrono::{Duration, Utc};
 use std::sync::Arc;
 
-use crate::domain::repositories::{
-    adventurers::AdventurerRepository, guild_commanders::GuildCommanderRepository,
+use crate::{
+    config::config_loader::{get_adventurer_secret, get_guild_commander_secret},
+    domain::repositories::{
+        adventurers::AdventurerRepository, guild_commanders::GuildCommanderRepository,
+    },
+    infrastructure::{
+        argon2_hashing,
+        jwt_authentication::{
+            self,
+            authentication_model::LoginModel,
+            jwt_model::{Claims, Passport, Roles},
+        },
+    },
 };
 
 pub struct AuthenticationUseCase<T1, T2>
@@ -26,19 +38,143 @@ where
         }
     }
 
-    pub async fn adventurer_login(&self, username: String, password: String) -> Result<i32> {
-        unimplemented!()
+    pub async fn adventurer_login(&self, login_model: LoginModel) -> Result<Passport> {
+        let secret_env = get_adventurer_secret()?;
+
+        let adventurer = self
+            .adventurer_repository
+            .find_by_username(login_model.username.clone())
+            .await?;
+
+        if !argon2_hashing::verify(login_model.password, adventurer.password)? {
+            return Err(anyhow::anyhow!("Invalid password"));
+        }
+
+        let access_token_claims = Claims {
+            sub: adventurer.id.to_string(),
+            role: Roles::Adventurer,
+            exp: (Utc::now() + Duration::days(1)).timestamp() as usize,
+            iat: Utc::now().timestamp() as usize,
+        };
+
+        let refresh_token_claims = Claims {
+            sub: adventurer.id.to_string(),
+            role: Roles::Adventurer,
+            exp: (Utc::now() + Duration::days(7)).timestamp() as usize,
+            iat: Utc::now().timestamp() as usize,
+        };
+
+        let access_token =
+            jwt_authentication::generate_token(secret_env.secret, &access_token_claims)?;
+
+        let refresh_token =
+            jwt_authentication::generate_token(secret_env.refresh_secret, &refresh_token_claims)?;
+
+        Ok(Passport {
+            access_token,
+            refresh_token,
+        })
     }
 
-    pub async fn adventurer_refresh_token(&self, refresh_token: String) -> Result<i32> {
-        unimplemented!()
+    pub async fn adventurer_refresh_token(&self, refresh_token: String) -> Result<Passport> {
+        let secret_env = get_adventurer_secret()?;
+
+        let claims =
+            jwt_authentication::verify_token(secret_env.refresh_secret.clone(), refresh_token)?;
+
+        let access_token_claims = Claims {
+            sub: claims.sub.clone(),
+            role: Roles::Adventurer,
+            exp: (Utc::now() + Duration::days(1)).timestamp() as usize,
+            iat: Utc::now().timestamp() as usize,
+        };
+
+        let refresh_token_claims = Claims {
+            sub: claims.sub,
+            role: Roles::Adventurer,
+            exp: claims.exp,
+            iat: Utc::now().timestamp() as usize,
+        };
+
+        let access_token =
+            jwt_authentication::generate_token(secret_env.secret, &access_token_claims)?;
+
+        let refresh_token =
+            jwt_authentication::generate_token(secret_env.refresh_secret, &refresh_token_claims)?;
+
+        Ok(Passport {
+            access_token,
+            refresh_token,
+        })
     }
 
-    pub async fn guild_commander_login(&self, username: String, password: String) -> Result<i32> {
-        unimplemented!()
+    pub async fn guild_commander_login(&self, login_model: LoginModel) -> Result<Passport> {
+        let secret_env = get_guild_commander_secret()?;
+
+        let guild_commander = self
+            .guild_commander_repository
+            .find_by_username(login_model.username.clone())
+            .await?;
+
+        if !argon2_hashing::verify(login_model.password, guild_commander.password)? {
+            return Err(anyhow::anyhow!("Invalid password"));
+        }
+
+        let access_token_claims = Claims {
+            sub: guild_commander.id.to_string(),
+            role: Roles::GuildCommander,
+            exp: (Utc::now() + Duration::days(1)).timestamp() as usize,
+            iat: Utc::now().timestamp() as usize,
+        };
+
+        let refresh_token_claims = Claims {
+            sub: guild_commander.id.to_string(),
+            role: Roles::GuildCommander,
+            exp: (Utc::now() + Duration::days(7)).timestamp() as usize,
+            iat: Utc::now().timestamp() as usize,
+        };
+
+        let access_token =
+            jwt_authentication::generate_token(secret_env.secret, &access_token_claims)?;
+
+        let refresh_token =
+            jwt_authentication::generate_token(secret_env.refresh_secret, &refresh_token_claims)?;
+
+        Ok(Passport {
+            access_token,
+            refresh_token,
+        })
     }
 
-    pub async fn guild_commander_refresh_token(&self, refresh_token: String) -> Result<i32> {
-        unimplemented!()
+    pub async fn guild_commander_refresh_token(&self, refresh_token: String) -> Result<Passport> {
+        let secret_env = get_guild_commander_secret()?;
+
+        let claims =
+            jwt_authentication::verify_token(secret_env.refresh_secret.clone(), refresh_token)?;
+
+        let access_token_claims = Claims {
+            sub: claims.sub.clone(),
+            role: Roles::GuildCommander,
+            exp: (Utc::now() + Duration::days(1)).timestamp() as usize,
+            iat: Utc::now().timestamp() as usize,
+        };
+
+        let refresh_token_claims = Claims {
+            sub: claims.sub,
+            role: Roles::GuildCommander,
+            exp: claims.exp,
+            iat: Utc::now().timestamp() as usize,
+        };
+
+        let access_token =
+            jwt_authentication::generate_token(secret_env.secret, &access_token_claims)?;
+
+        let refresh_token =
+            jwt_authentication::generate_token(secret_env.refresh_secret, &refresh_token_claims)?;
+
+        Ok(Passport {
+            access_token,
+            refresh_token,
+        })
     }
 }
--- src/infrastructure/axum_http/routers/authentication.rs
+++ src/infrastructure/axum_http/routers/authentication.rs
@@ -1,13 +1,237 @@
 use std::sync::Arc;
 
-use axum::{response::IntoResponse, routing::post, Router};
+use axum::{
+    extract::State,
+    http::{header, HeaderMap, StatusCode},
+    response::IntoResponse,
+    routing::post,
+    Json, Router,
+};
+use axum_extra::extract::cookie::{Cookie, CookieJar};
+use cookie::time::Duration;
 
-use crate::infrastructure::postgres::postgres_connection::PgPoolSquad;
+use crate::{
+    application::usecases::authentication::AuthenticationUseCase,
+    config::{config_loader::get_stage, stage::Stage},
+    domain::repositories::{
+        adventurers::AdventurerRepository, guild_commanders::GuildCommanderRepository,
+    },
+    infrastructure::{
+        jwt_authentication::authentication_model::LoginModel,
+        postgres::{
+            postgres_connection::PgPoolSquad,
+            repositories::{
+                adventurers::AdventurerPostgres, guild_commanders::GuildCommanderPostgres,
+            },
+        },
+    },
+};
 
 pub fn routes(db_pool: Arc<PgPoolSquad>) -> Router {
-    Router::new().route("/login", post(login))
+    let adventurer_repository = Arc::new(AdventurerPostgres::new(Arc::clone(&db_pool)));
+    let guild_commander_repository = Arc::new(GuildCommanderPostgres::new(Arc::clone(&db_pool)));
+
+    let authentication_usecase =
+        AuthenticationUseCase::new(adventurer_repository, guild_commander_repository);
+
+    Router::new()
+        .route("/adventurers/login", post(adventurer_login))
+        .route("/guild-commanders/login", post(guild_commander_login))
+        .route("/adventurers/refresh-token", post(adventurer_refresh_token))
+        .route(
+            "/guild-commanders/refresh-token",
+            post(guild_commander_refresh_token),
+        )
+        .with_state(Arc::new(authentication_usecase))
 }
 
-pub async fn login() -> impl IntoResponse {
-    unimplemented!()
+pub async fn adventurer_login<T1, T2>(
+    State(authentication_usecase): State<Arc<AuthenticationUseCase<T1, T2>>>,
+    Json(login_model): Json<LoginModel>,
+) -> impl IntoResponse
+where
+    T1: AdventurerRepository + Send + Sync,
+    T2: GuildCommanderRepository + Send + Sync,
+{
+    match authentication_usecase.adventurer_login(login_model).await {
+        Ok(passport) => {
+            let mut access_token_cookie = Cookie::build(("act", passport.access_token.clone()))
+                .path("/")
+                .same_site(cookie::SameSite::Lax)
+                .http_only(true)
+                .max_age(Duration::days(14));
+
+            let mut refresh_token_cookie = Cookie::build(("rft", passport.refresh_token.clone()))
+                .path("/")
+                .same_site(cookie::SameSite::Lax)
+                .http_only(true)
+                .max_age(Duration::days(14));
+
+            if get_stage() == Stage::Production {
+                access_token_cookie = access_token_cookie.secure(true);
+                refresh_token_cookie = refresh_token_cookie.secure(true);
+            }
+
+            let mut headers = HeaderMap::new();
+            headers.append(
+                header::SET_COOKIE,
+                header::HeaderValue::from_str(&access_token_cookie.to_string()).unwrap(),
+            );
+            headers.append(
+                header::SET_COOKIE,
+                header::HeaderValue::from_str(&refresh_token_cookie.to_string()).unwrap(),
+            );
+
+            (StatusCode::OK, headers, "Login successfully").into_response()
+        }
+        Err(e) => (StatusCode::UNAUTHORIZED, e.to_string()).into_response(),
+    }
+}
+
+pub async fn adventurer_refresh_token<T1, T2>(
+    State(authentication_usecase): State<Arc<AuthenticationUseCase<T1, T2>>>,
+    jar: CookieJar,
+) -> impl IntoResponse
+where
+    T1: AdventurerRepository + Send + Sync,
+    T2: GuildCommanderRepository + Send + Sync,
+{
+    if let Some(rft) = jar.get("rft") {
+        match authentication_usecase
+            .adventurer_refresh_token(rft.value().to_string())
+            .await
+        {
+            Ok(passport) => {
+                let mut access_token_cookie = Cookie::build(("act", passport.access_token.clone()))
+                    .path("/")
+                    .same_site(cookie::SameSite::Lax)
+                    .http_only(true)
+                    .max_age(Duration::days(14));
+
+                let mut refresh_token_cookie =
+                    Cookie::build(("rft", passport.refresh_token.clone()))
+                        .path("/")
+                        .same_site(cookie::SameSite::Lax)
+                        .http_only(true)
+                        .max_age(Duration::days(14));
+
+                if get_stage() == Stage::Production {
+                    access_token_cookie = access_token_cookie.secure(true);
+                    refresh_token_cookie = refresh_token_cookie.secure(true);
+                }
+
+                let mut headers = HeaderMap::new();
+                headers.append(
+                    header::SET_COOKIE,
+                    header::HeaderValue::from_str(&access_token_cookie.to_string()).unwrap(),
+                );
+                headers.append(
+                    header::SET_COOKIE,
+                    header::HeaderValue::from_str(&refresh_token_cookie.to_string()).unwrap(),
+                );
+
+                (StatusCode::OK, headers, "Login successfully").into_response()
+            }
+            Err(e) => (StatusCode::UNAUTHORIZED, e.to_string()).into_response(),
+        }
+    } else {
+        (StatusCode::BAD_REQUEST, "Refresh token not found").into_response()
+    }
+}
+
+pub async fn guild_commander_login<T1, T2>(
+    State(authentication_usecase): State<Arc<AuthenticationUseCase<T1, T2>>>,
+    Json(login_model): Json<LoginModel>,
+) -> impl IntoResponse
+where
+    T1: AdventurerRepository + Send + Sync,
+    T2: GuildCommanderRepository + Send + Sync,
+{
+    match authentication_usecase
+        .guild_commander_login(login_model)
+        .await
+    {
+        Ok(passport) => {
+            let mut access_token_cookie = Cookie::build(("act", passport.access_token.clone()))
+                .path("/")
+                .same_site(cookie::SameSite::Lax)
+                .http_only(true)
+                .max_age(Duration::days(14));
+
+            let mut refresh_token_cookie = Cookie::build(("rft", passport.refresh_token.clone()))
+                .path("/")
+                .same_site(cookie::SameSite::Lax)
+                .http_only(true)
+                .max_age(Duration::days(14));
+
+            if get_stage() == Stage::Production {
+                access_token_cookie = access_token_cookie.secure(true);
+                refresh_token_cookie = refresh_token_cookie.secure(true);
+            }
+
+            let mut headers = HeaderMap::new();
+            headers.append(
+                header::SET_COOKIE,
+                header::HeaderValue::from_str(&access_token_cookie.to_string()).unwrap(),
+            );
+            headers.append(
+                header::SET_COOKIE,
+                header::HeaderValue::from_str(&refresh_token_cookie.to_string()).unwrap(),
+            );
+
+            (StatusCode::OK, headers, "Login successfully").into_response()
+        }
+        Err(e) => (StatusCode::UNAUTHORIZED, e.to_string()).into_response(),
+    }
+}
+
+pub async fn guild_commander_refresh_token<T1, T2>(
+    State(authentication_usecase): State<Arc<AuthenticationUseCase<T1, T2>>>,
+    jar: CookieJar,
+) -> impl IntoResponse
+where
+    T1: AdventurerRepository + Send + Sync,
+    T2: GuildCommanderRepository + Send + Sync,
+{
+    if let Some(rft) = jar.get("rft") {
+        match authentication_usecase
+            .guild_commander_refresh_token(rft.value().to_string())
+            .await
+        {
+            Ok(passport) => {
+                let mut access_token_cookie = Cookie::build(("act", passport.access_token.clone()))
+                    .path("/")
+                    .same_site(cookie::SameSite::Lax)
+                    .http_only(true)
+                    .max_age(Duration::days(14));
+
+                let mut refresh_token_cookie =
+                    Cookie::build(("rft", passport.refresh_token.clone()))
+                        .path("/")
+                        .same_site(cookie::SameSite::Lax)
+                        .http_only(true)
+                        .max_age(Duration::days(14));
+
+                if get_stage() == Stage::Production {
+                    access_token_cookie = access_token_cookie.secure(true);
+                    refresh_token_cookie = refresh_token_cookie.secure(true);
+                }
+
+                let mut headers = HeaderMap::new();
+                headers.append(
+                    header::SET_COOKIE,
+                    header::HeaderValue::from_str(&access_token_cookie.to_string()).unwrap(),
+                );
+                headers.append(
+                    header::SET_COOKIE,
+                    header::HeaderValue::from_str(&refresh_token_cookie.to_string()).unwrap(),
+                );
+
+                (StatusCode::OK, headers, "Login successfully").into_response()
+            }
+            Err(e) => (StatusCode::UNAUTHORIZED, e.to_string()).into_response(),
+        }
+    } else {
+        (StatusCode::BAD_REQUEST, "Refresh token not found").into_response()
+    }
 }
