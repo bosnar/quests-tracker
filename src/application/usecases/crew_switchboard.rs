5e614e0988e920e42d32c1a6215bc0ab8fae696e
bosnar <dev.chadchai@gmail.comwd>
2025-04-13 09:17:12 +0700

    feat: join and leave quest

--- src/application/usecases/crew_switchboard.rs
+++ src/application/usecases/crew_switchboard.rs
@@ -1,8 +1,14 @@
-use anyhow::Result;
+use anyhow::{Ok, Result};
 use std::sync::Arc;
 
-use crate::domain::repositories::{
-    crew_switchboard::CrewSwitchboardRepository, quest_viewing::QuestViewingRepository,
+use crate::domain::{
+    repositories::{
+        crew_switchboard::CrewSwitchboardRepository, quest_viewing::QuestViewingRepository,
+    },
+    value_objects::{
+        quest_adventurer_junction::{QuestAdventurerJunction, MAX_ADVENTURERS_PER_QUEST},
+        quest_statuses::QuestStatuses,
+    },
 };
 
 pub struct CrewSwitchboardUseCase<T1, T2>
@@ -27,10 +33,52 @@ where
     }
 
     pub async fn join(&self, quest_id: i32, adventurer_id: i32) -> Result<()> {
-        unimplemented!()
-    }
+        let quest = self.quest_viewing_repository.view_details(quest_id).await?;
 
+        let adventurer_count = self
+            .quest_viewing_repository
+            .adventurer_counting_by_quest_id(quest_id)
+            .await?;
+
+        let quest_status_condition = quest.status == QuestStatuses::Open.to_string()
+            || quest.status == QuestStatuses::Failed.to_string();
+
+        let adventurers_count_condition = adventurer_count < MAX_ADVENTURERS_PER_QUEST;
+
+        if !adventurers_count_condition {
+            return Err(anyhow::anyhow!("Quest is full"));
+        }
+
+        if !quest_status_condition {
+            return Err(anyhow::anyhow!("Quest is not started"));
+        }
+
+        self.crew_switchboard_repository
+            .join(QuestAdventurerJunction {
+                quest_id,
+                adventurer_id,
+            })
+            .await?;
+
+        Ok(())
+    }
     pub async fn leave(&self, quest_id: i32, adventurer_id: i32) -> Result<()> {
-        unimplemented!()
+        let quest = self.quest_viewing_repository.view_details(quest_id).await?;
+
+        let leave_condition = quest.status == QuestStatuses::Open.to_string()
+            || quest.status == QuestStatuses::Failed.to_string();
+
+        if !leave_condition {
+            return Err(anyhow::anyhow!("Quest is not leaveable"));
+        }
+
+        self.crew_switchboard_repository
+            .leave(QuestAdventurerJunction {
+                quest_id,
+                adventurer_id,
+            })
+            .await?;
+
+        Ok(())
     }
 }
--- src/infrastructure/axum_http/routers/crew_switchboard.rs
+++ src/infrastructure/axum_http/routers/crew_switchboard.rs
@@ -2,6 +2,7 @@ use std::sync::Arc;
 
 use axum::{
     extract::{Path, State},
+    http::StatusCode,
     middleware,
     response::IntoResponse,
     routing::{delete, post},
@@ -48,7 +49,10 @@ where
     T1: CrewSwitchboardRepository + Send + Sync,
     T2: QuestViewingRepository + Send + Sync,
 {
-    unimplemented!()
+    match crew_switchboard_usecase.join(quest_id, adventurer_id).await {
+        Ok(_) => (StatusCode::OK, String::from("Joined quest successfully")),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+    }
 }
 
 pub async fn leave<T1, T2>(
@@ -60,5 +64,11 @@ where
     T1: CrewSwitchboardRepository + Send + Sync,
     T2: QuestViewingRepository + Send + Sync,
 {
-    unimplemented!()
+    match crew_switchboard_usecase
+        .leave(quest_id, adventurer_id)
+        .await
+    {
+        Ok(_) => (StatusCode::OK, String::from("Left quest successfully")),
+        Err(e) => (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()),
+    }
 }
